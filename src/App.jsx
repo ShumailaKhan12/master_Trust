1f966618d277787d75727872f964574eed706786
Ishra Khanam <ishra6107@gmail.com>
2025-09-17 16:41:25 +0530

    Sidebar created
    Sidebar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import './App.css'
-
+import Home from './pages/home/home'
 function App() {
 
   return (
     <>
-     <div className="flex h-screen items-center justify-center bg-gray-300">
-      <p className="text-4xl font-bold text-red-600">
-        Hello React + Vite + Tailwind
-      </p>
-    </div>
+    <Home />
     </>
-  )
+  ) 
 }
 
 export default App
