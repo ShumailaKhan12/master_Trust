e358301c5b6b3b34f5f78d632e318a9d05d1832d
ShumailaKhan12 <shumaila1641@gmail.com>
2025-09-17 17:04:37 +0530

    fix routes

--- src/routes/appRoutes/index.jsx
+++ src/routes/appRoutes/index.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../../pages/home/home';
 import Login from '../../pages/auth/login/login';
 import Register from '../../pages/auth/register/register';
+import TopPicks from '../../pages/portfolio/portfolio';
+import FundsPanel from '../../pages/funds/funds';
 
 const AppRoutes = () => {
     return (
@@ -10,6 +12,8 @@ const AppRoutes = () => {
             <Router>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/portfolio" element={<TopPicks />} />
+                    <Route path="/fund" element={<FundsPanel />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                 </Routes>
