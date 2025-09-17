cb0634eb0eb42e6723894418d02bddeb90c12a47
JunedAbbasi <abbasijuned632@gmail.com>
2025-09-17 16:44:58 +0530

    fund and portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import './App.css'
+import Portfolio from "./pages/portfolio/portfolio"
+import FundsPanel from './pages/funds/funds'
 
 function App() {
 
   return (
     <>
-     <div className="flex h-screen items-center justify-center bg-gray-300">
+    <Portfolio/>
+    <FundsPanel/>
+     {/* <div className="flex h-screen items-center justify-center bg-gray-300">
       <p className="text-4xl font-bold text-red-600">
         Hello React + Vite + Tailwind
       </p>
-    </div>
+    </div> */}
     </>
   )
 }
