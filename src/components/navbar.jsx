6b3444969a252c5116bcf0d9409efa4297749b28
Ishra Khanam <ishra6107@gmail.com>
2025-09-17 18:05:32 +0530

    Profile modal open added
    Profile modal open added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,99 +1,14 @@
-// import React, { useState } from 'react'
-
-// import { NavLink } from "react-router-dom";
-// import { IoMenu } from 'react-icons/io5';
-// import { RxCross2 } from 'react-icons/rx';
-// import MasterLogo from '../assets/Images/master-logo.png'
-// import { BsChevronBarDown } from 'react-icons/bs';
-// import { ChevronDown } from 'lucide-react';
-// const Navbar = () => {
-//     const [isOpen, setIsOpen] = useState(false);
-
-//     return (
-//         <>
-//             <nav className="w-full bg-white border-b shadow-sm px-6 py-2 flex items-center fixed z-10">
-//                 {/* Left Section */}
-//                 <div className="flex items-center space-x-6 w-110">
-//                     {/* NIFTY & SENSEX */}
-//                     <div className="hidden md:flex items-center space-x-8 text-sm">
-//                         <div className="flex space-x-1 flex-col">
-//                             <span className="font-semibold">NIFTY</span>
-//                             <div>
-//                                 <span>25334.15</span>
-//                                 <span className="text-green-600">+95.05 (+0.38%)</span>
-//                             </div>
-//                         </div>
-//                         <div className="flex space-x-1 flex-col">
-//                             <span className="font-semibold">SENSEX</span>
-//                             <div>
-//                                 <span>82713.14</span>
-//                                 <span className="text-green-600">+332.45 (+0.40%)</span>
-//                             </div>
-//                         </div>
-//                         <div>
-//                             <ChevronDown />
-//                         </div>
-//                     </div>
-//                 </div>
-
-//                 <div className='flex justify-between'>
-//                     {/* Center Logo */}
-//                     <div className="">
-//                         <img src={MasterLogo} alt="Logo" className='w-24 h-14' />
-//                     </div>
-
-
-//                     <div className="hidden md:flex items-center space-x-6">
-//                         <NavLink to="/" className={({ isActive }) => isActive ? "text-blue-600 font-medium" : "text-gray-600 hover:text-blue-600"}>Home</NavLink>
-//                         <NavLink to="/portfolio" className={({ isActive }) => isActive ? "text-blue-600 font-medium" : "text-gray-600 hover:text-blue-600"}>Portfolio</NavLink>
-//                         <NavLink to="/funds" className={({ isActive }) => isActive ? "text-blue-600 font-medium" : "text-gray-600 hover:text-blue-600"}>Funds</NavLink>
-//                         <div className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100 text-blue-700 font-semibold cursor-pointer">
-//                             R
-//                         </div>
-//                     </div>
-
-//                 </div>
-
-
-
-//                 {/* Mobile Menu Toggle */}
-//                 <div className="md:hidden flex items-center">
-//                     <button onClick={() => setIsOpen(!isOpen)}>
-//                         {isOpen ? <RxCross2 className="w-6 h-6" /> : <IoMenu className="w-6 h-6" />}
-//                     </button>
-//                 </div>
-
-//                 {/* Mobile Menu */}
-//                 {isOpen && (
-//                     <div className="absolute top-14 left-0 w-full bg-white shadow-md flex flex-col items-center py-4 space-y-4 md:hidden z-50">
-//                         <NavLink to="/" onClick={() => setIsOpen(false)} className={({ isActive }) => isActive ? "text-blue-600 font-medium" : "text-gray-600 hover:text-blue-600"}>Home</NavLink>
-//                         <NavLink to="/portfolio" onClick={() => setIsOpen(false)} className={({ isActive }) => isActive ? "text-blue-600 font-medium" : "text-gray-600 hover:text-blue-600"}>Portfolio</NavLink>
-//                         <NavLink to="/funds" onClick={() => setIsOpen(false)} className={({ isActive }) => isActive ? "text-blue-600 font-medium" : "text-gray-600 hover:text-blue-600"}>Funds</NavLink>
-//                         <div className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100 text-blue-700 font-semibold cursor-pointer">
-//                             R
-//                         </div>
-//                     </div>
-//                 )}
-//             </nav>
-
-//         </>
-//     );
-// };
-
-// export default Navbar
-
-
 import React, { useState } from 'react'
 import { NavLink } from "react-router-dom";
 import { IoMenu } from 'react-icons/io5';
 import { RxCross2 } from 'react-icons/rx';
 import MasterLogo from '../assets/Images/master-logo.png'
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown, ChevronUp, LogOut } from 'lucide-react';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
+    const [isProfileOpen, setIsProfileOpen] = useState(false);
     return (
         <>
             <nav className="w-full bg-white border-b shadow-sm px-6 py-2 flex items-center fixed z-10">
@@ -122,7 +37,7 @@ const Navbar = () => {
                                 onClick={() => setIsDropdownOpen(!isDropdownOpen)}
                                 className="flex items-center text-gray-700 hover:text-blue-600"
                             >
-                                <ChevronDown />
+                                <ChevronUp />
                             </button>
 
                             {/* Dropdown list */}
@@ -138,7 +53,7 @@ const Navbar = () => {
                 </div>
 
                 {/* Center Logo */}
-                <div className="flex justify-start ml-10 flex-grow">
+                <div className="flex justify-start ml-12 flex-grow">
                     <img src={MasterLogo} alt="Logo" className='w-24 h-14' />
                 </div>
 
@@ -147,7 +62,7 @@ const Navbar = () => {
                     <NavLink to="/" className={({ isActive }) => isActive ? "text-blue-600 font-medium" : "text-gray-600 hover:text-blue-600"}>Home</NavLink>
                     <NavLink to="/portfolio" className={({ isActive }) => isActive ? "text-blue-600 font-medium" : "text-gray-600 hover:text-blue-600"}>Portfolio</NavLink>
                     <NavLink to="/funds" className={({ isActive }) => isActive ? "text-blue-600 font-medium" : "text-gray-600 hover:text-blue-600"}>Funds</NavLink>
-                    <div className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100 text-blue-700 font-semibold cursor-pointer">
+                    <div onClick={() => setIsProfileOpen(!isProfileOpen)} className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100 text-blue-700 font-semibold cursor-pointer">
                         R
                     </div>
                 </div>
@@ -166,8 +81,44 @@ const Navbar = () => {
                     <NavLink to="/" onClick={() => setIsOpen(false)} className={({ isActive }) => isActive ? "text-blue-600 font-medium" : "text-gray-600 hover:text-blue-600"}>Home</NavLink>
                     <NavLink to="/portfolio" onClick={() => setIsOpen(false)} className={({ isActive }) => isActive ? "text-blue-600 font-medium" : "text-gray-600 hover:text-blue-600"}>Portfolio</NavLink>
                     <NavLink to="/funds" onClick={() => setIsOpen(false)} className={({ isActive }) => isActive ? "text-blue-600 font-medium" : "text-gray-600 hover:text-blue-600"}>Funds</NavLink>
-                    <div className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100 text-blue-700 font-semibold cursor-pointer">
+                    <div onClick={() => setIsProfileOpen(!isProfileOpen)} className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100 text-blue-700 font-semibold cursor-pointer">
                         R
+                    </div>
+                </div>
+            )}
+
+
+
+            {isProfileOpen && (
+                <div className="fixed inset-0 flex justify-end z-50 pt-20 pr-5">
+                    {/* Overlay click → close */}
+                    <div
+                        className="absolute inset-0"
+                        onClick={() => setIsProfileOpen(false)}
+                    ></div>
+
+                    {/* Profile Drawer */}
+                    <div className="relative w-64 bg-white h-48 shadow-lg p-6 flex flex-col z-50">
+                        {/* User Info */}
+                        <div className="mb-6">
+                            <h2 className="text-lg font-semibold">Hi, Rajik</h2>
+                        </div>
+
+                        {/* Logout */}
+
+                        <div className='flex items-center'>
+                            <LogOut />
+                             <button
+                            className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+                            onClick={() => {
+                                // logout logic
+                                setIsProfileOpen(false);
+                            }}
+                        >
+                            Logout
+                        </button>
+                        </div>
+                       
                     </div>
                 </div>
             )}
