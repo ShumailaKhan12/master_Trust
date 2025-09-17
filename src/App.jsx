41e9da76dacaefaf5ab6113c568db2ee0c481bca
Ishra Khanam <ishra6107@gmail.com>
2025-09-17 16:52:53 +0530

    Fixed navbar error
    Fixed navbar error

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './App.css'
 import Login from './pages/auth/login/login'
 import Home from './pages/home/home'
+import { BrowserRouter } from 'react-router-dom'
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       {/* <Login /> */}
       <Home/>
-    </>
+    </BrowserRouter>
   ) 
 }
 
