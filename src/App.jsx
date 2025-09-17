3beb95758b307a7fbd2d89369dcd9b91ac4f58b6
ShumailaKhan12 <shumaila1641@gmail.com>
2025-09-17 14:32:58 +0530

    Start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="flex h-screen items-center justify-center bg-gray-100">
+      <p className="text-4xl font-bold text-red-600">
+        Hello React + Vite + Tailwind
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
