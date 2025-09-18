c94fce3636f79afa3e10c3894bd452de7df9438f
ShumailaKhan12 <shumaila1641@gmail.com>
2025-09-18 20:33:57 +0530

    fixx

--- src/components/MarketMovers.jsx
+++ src/components/MarketMovers.jsx
@@ -31,7 +31,6 @@ const MarketMovers = () => {
                 onClick={() => setActiveTab('Top gainers & losers')}
                 className={`py-4 px-1 border-b-2 font-medium text-sm ${activeTab === 'Top gainers & losers'
                   ? 'border-blue text-blue'
-                  ? 'border-blue text-blue'
                   : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
                   }`}
               >
